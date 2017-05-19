4329aff8e3474091ccd6353a5216ff8bfa9692f5
GordonCoffeeCan <gordon.lee.6170@gmail.com>
2017-05-19 11:37:15 -0400

    start

---
+++ LearnCSharp/LearnCSharp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnCSharp {
+    class Program {
+        static void Main(string[] args) {
+            Vector3 _vector3 = new Vector3();
+
+            //_vector3.x = 1;
+            //_vector3.y = 1;
+            //_vector3.z = 1;
+
+            //_vector3.SetX(1);
+            //_vector3.SetY(1);
+            //_vector3.SetZ(1);
+
+            
+
+            Console.WriteLine(_vector3.Length());
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ LearnCSharp/LearnCSharp/Vector3.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnCSharp {
+    class Vector3 {
+
+        private float x, y, z, length;
+
+        public Vector3() {
+            Console.WriteLine("run");
+        }
+
+        public Vector3(float _x, float _y, float _z) {
+            this.x = _x;
+            this.y = _y;
+            this.z = _z;
+
+            length = Length();
+        }
+
+        public float Length() {
+            return (float)Math.Sqrt(x * x + y * y + z * z);
+        }
+
+    }
+}
