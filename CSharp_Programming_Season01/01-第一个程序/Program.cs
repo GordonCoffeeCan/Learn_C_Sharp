6c198703b081a606f685e7639b659a76c81bb7ca
GL <gordon.lee.6170@gmail.com>
2020-09-18 22:09:51 -0700

    reset project. reviewed to while loop

---
+++ CSharp_Programming_Season01/01-第一个程序/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _01_第一个程序
+{
+    class Program
+    {
+        static void Main() {
+            int myInt = 100;
+            string testStr;
+            String resString = myInt < 10 ? "yes" : "no";
+
+            Console.WriteLine(resString);
+
+            switch (myInt) {
+                case 1:
+                    testStr = "not 1";
+                    break;
+
+                case 2:
+                    testStr = "not 2";
+                    break;
+
+                default:
+                    testStr = "you can't guess";
+                    break;
+            }
+
+            Console.WriteLine(testStr);
+
+        }
+    }
+}
---
+++ CSharp_Programming_Season01/024-While循环/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _024_While循环
+{
+    class Program
+    {
+        static void Main(string[] args) {
+
+            int myNum = 1;
+            while (myNum <= 9) {
+                Console.WriteLine(myNum);
+                myNum++;
+            }
+        }
+    }
+}
