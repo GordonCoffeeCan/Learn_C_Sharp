d4d4c9572b118dbf08db67323180255ca0ffc77b
gordonlee <gordon.lee.6170@gmail.com>
2020-10-04 22:42:50 -0700

    start season 2

---
+++ CSharp_Programming_Season02/01-调试和错误处理/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _01_调试和错误处理 {
+    class Program {
+        static void Main(string[] args) {
+            //int temp = 234;
+
+            int num1 = 34;
+            int num2 = 45;
+            int sum = num1 + num2;
+        }
+    }
+}
