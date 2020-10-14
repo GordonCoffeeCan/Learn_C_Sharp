a07233f41357e63c06a81a1696cf18e9b1d93f91
gordonlee <gordon.lee.6170@gmail.com>
2020-10-13 22:48:08 -0700

    generic class

---
+++ CSharp_Programming_Season02/17-泛型-泛型类/ClassA.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _17_泛型_泛型类 {
+    class ClassA<T> {//T代表一个数据类型，使用ClassA构造时候要确定T的类型
+
+        private T a;
+        private T b;
+        
+        public ClassA(T a, T b) {
+            this.a = a;
+            this.b = b;
+        }
+
+        public string GetSum() {
+            return a + " " + b;
+        }
+
+    }
+}
---
+++ CSharp_Programming_Season02/17-泛型-泛型类/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _17_泛型_泛型类 {
+    class Program {
+        static void Main(string[] args) {
+            ClassA<int> o1 = new ClassA<int>(12, 15); //泛型类构造时候需要指定泛型的类型；
+
+            ClassA<string> o2 = new ClassA<string>("test", "new");
+
+            string s = o1.GetSum();
+            string s2 = o2.GetSum();
+            Console.WriteLine(s2);
+        }
+    }
+}
